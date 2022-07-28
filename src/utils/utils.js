c0c8c41afd049c9b1054f42f1a11a8b0677f0a7c
talCOhenGit <tulis.levi@gmail.com>
2022-07-28 19:46:22 +0300

    handling data to send the backend

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,4 @@
-import { numOfColBeforeProducts, numOfColAfterProducts } from "./constants"
+import { numOfColBeforeProducts, numOfColAfterProducts } from "./constants";
 
 export const filterCustomers = (parsedName, input) => {
   return input.length && parsedName?.length && parsedName.includes(input);
@@ -9,7 +9,7 @@ export const mapTable = (data, key) =>
     if (key !== "מערך מאופס") {
       return element[key];
     } else {
-      return 0 ;
+      return 0;
     }
   });
 
@@ -31,7 +31,7 @@ export const createBalanceTable = (data) => {
       null,
       null,
       null,
-      null
+      null,
     ];
     currentBalanceData.push(tableRowData);
   });
@@ -39,36 +39,59 @@ export const createBalanceTable = (data) => {
 };
 
 export const handleMatrixData = (tableData, productsMap) => {
-  let matrix = JSON.parse(JSON.stringify(tableData))
-  const titleLength = matrix[0].length
-  const products = matrix[0].slice(numOfColBeforeProducts, titleLength-numOfColAfterProducts)
-  const productsWithKeys = products.map(element => 
-    productsMap[element]
-  )
-  let tableDetails = matrix.slice(1)
-  const metaData = []
-  const documentIDs = []
-  const actionIDs = []
-  const driverIDs = []
-  const docComments = []
+  let matrix = JSON.parse(JSON.stringify(tableData));
+  const titleLength = matrix[0].length;
+  const products = matrix[0].slice(
+    numOfColBeforeProducts,
+    titleLength - numOfColAfterProducts
+  );
+  const productsWithKeys = products.map((element) => productsMap[element]);
+  let tableDetails = matrix.slice(1);
+  const metaData = [];
+  const documentIDs = [];
+  const actionIDs = [];
+  const driverIDs = [];
+  const docComments = [];
   tableDetails.map((rowData) => {
-    docComments.push(rowData.pop())
-    metaData.push(rowData.pop())
-    documentIDs.push(rowData.pop())
-    actionIDs.push(rowData.pop())
-    driverIDs.push(rowData.pop())
-  })
-  matrix = [productsWithKeys, ...tableDetails]
-  matrix[0].unshift("AcountName", "AountKey", "CellPhone")
-  return {matrix, driverIDs, actionIDs, documentIDs, metaData, docComments}
-}
+    docComments.push(rowData.pop());
+    metaData.push(rowData.pop());
+    documentIDs.push(rowData.pop());
+    actionIDs.push(rowData.pop());
+    driverIDs.push(rowData.pop());
+  });
+  matrix = [productsWithKeys, ...tableDetails];
+  matrix[0].unshift("AcountName", "AountKey", "CellPhone");
+  return { matrix, driverIDs, actionIDs, documentIDs, metaData, docComments };
+};
 
 export const handleChangedMatrixData = (matrixComments, docComments) => {
-  console.log("matrixComments and docComments", matrixComments, docComments)
-}
+  console.log(" handleChangedMatrixDatamatrixComments and docComments", matrixComments, docComments);
+  const matrixCommentToSend = []
+  matrixComments.forEach((commentsRow, index) => {
+    let cellsData = [];
+    commentsRow.forEach((comments) => {
+      if (!comments) {
+        cellsData.push(null);
+      } else {
+        const commentsObj = {};
+        comments.forEach(
+          (comment) =>
+            (commentsObj[comment["selectValue"]] = comment["inputValue"])
+        );
+        cellsData.push(commentsObj);
+      }
+    });
+    console.log("cellsData:", cellsData)
+    const docData = !docComments[index]? null : docComments[index]
+    matrixCommentToSend.push({"cellsData": cellsData, "docData": docData})
+  });
+  return matrixCommentToSend
+};
 
 export const getProductsNameKeyMap = (products) => {
-  const productNameKey = {}
-  products.map((element) => productNameKey[element["שם פריט"]] = element["מפתח פריט"])
-  return productNameKey
-}
+  const productNameKey = {};
+  products.map(
+    (element) => (productNameKey[element["שם פריט"]] = element["מפתח פריט"])
+  );
+  return productNameKey;
+};
